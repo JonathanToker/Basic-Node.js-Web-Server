ae6cf8d40776f0ded22bd783ead5d0cf98adff3b
YonatanToker <yonatantoker123@gmail.com>
2024-05-22 17:12:59 +0300

    web server completed, serves html, css and a javascript file associated with the html file

--- app.ts
+++ app.ts
@@ -1,13 +1,58 @@
 import fs from "fs";
 import http from "http";
+import path from "path";
 const port = 3000;
+
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
-    res.writeHead(200, { "Content-Tye": "text/html" });
-    res.write("Hello Node");
-    res.end();
+    let filePath = "." + req.url;
+    if (filePath === "./") {
+      filePath = "./index.html";
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes: { [key: string]: string } = {
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".svg": "application/image/svg+xml",
+    };
+
+    const contentType = mimeTypes[extname] || "application/octet-stream";
+
+    fs.readFile(filePath, (error, content) => {
+      if (error) {
+        if (error.code === "ENOENT") {
+          fs.readFile("./404.html", (error404, content404) => {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(content404, "utf-8");
+          });
+        } else {
+          res.writeHead(500);
+          res.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n"
+          );
+        }
+      } else {
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(content, "utf-8");
+      }
+    });
   }
 );
+
 server.listen(port, (error?: Error) => {
   if (error) {
     console.log("Something went wrong", error);
---
+++ script.js
@@ -0,0 +1,4 @@
+const mainTitle = document.querySelector(".main-title");
+setTimeout(() => {
+  mainTitle.textContent = "Abungalo";
+}, 2000);
